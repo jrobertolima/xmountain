7ec78acffe58d8ca6369b5c57022018265b37b11
Joao Roberto de Lima <jbetol@gmail.com>
2017-09-27 19:57:43 -0300

    Dealing with db

--- sms4geek.rb
+++ sms4geek.rb
@@ -1,6 +1,68 @@
 require 'net/http'
-#require 'uri'
+require 'sqlite3'
 
+	def criadb
+		db = SQLite3::Database.new('xmountain.db')
+
+		rows = db.execute <<-SQL
+							create table IF NOT EXISTS atletas(
+							 	matricula INTEGER PRIMARY KEY,
+							 	nome varchar(50),
+							 	categoria varchar(10),
+							 	fone varchar(14)
+							 	);
+							 	SQL
+		return rows					 	
+	end
+
+	def insere(values)
+		db = SQLite3::Database.open('xmountain.db')
+
+		db.execute("INSERT INTO atletas(matricula, nome, categoria, fone) VALUES(?,?,?,?)", values)
+	end
+
+	def consulta
+		db = SQLite3::Database.open('xmountain')
+
+	end
+  res  = criadb
+  values = [[
+    "1702",
+		"Marya Lima",
+    "master a",
+    "31992320104"
+  ],
+  [
+    "1701",
+    "João Lima",
+    "master c",
+    "31992320098"
+  ],
+  [
+    "1704",
+    "Pedro Lima",
+    "cadete",
+    "31992320162"
+  ],
+  [
+    '1703',
+    "Tomaz Correa",
+    "Expert",
+    "3199500083"
+  ]
+  ]
+  values.each {|item| insere(item)}
+  #puts res
+	#puts res.inspect
+	
+
+
+
+=begin
+	
+rescue Exception => e
+	
+end
 username = 'jbetol'
 password = 'Petom@123'
 msisdn = ['31992320098','31992320104','31993500083']
@@ -24,4 +86,4 @@ end
 	  #   		'msisdn' => msisdn, 'msg' => msg})
 
 #p http.status
-
+=end
