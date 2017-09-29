771b8c6ef4efe9c9ebc9380bbb0c550756135944
Joao Roberto de Lima <jbetol@gmail.com>
2017-09-29 19:03:38 -0300

    Incluindo envio de mensagem

--- sms4geek.rb
+++ sms4geek.rb
@@ -2,6 +2,48 @@ require 'net/http'
 require 'sqlite3'
 require 'roo'
 
+def sendSms(atleta, categoria, tempo, fone, pos)
+  username = 'jbetol'
+  password = 'Petom@123'
+  msisdn = fone
+  msg = "XMountain Informa: 
+  #{atleta} 
+  Categoria #{categoria} 
+  Tempo de prova: #{tempo}
+  Classificação: /#{pos}"
+  puts msg
+
+  uri = URI('https://sms4geeks.appspot.com/smsgateway')#ction=out&username=YourUserName&password=YourPassword&msisdn=555123456&msg=hello')
+
+  request = Net::HTTP::Post.new(uri)
+	request.set_form_data('action' => 'out', 'username' => username, 'password' => password,
+		     		'msisdn' => msisdn, 'msg' => msg)
+
+	res = Net::HTTP.start(uri.hostname,  uri.port, :use_ssl => uri.scheme == 'http') do |http|
+		
+#		http.request(request)
+	#	puts "Res " + res.body
+	end	
+end
+
+def classifica(lista)
+  lista.size.to_s   
+end
+  
+#Consulta atleta dada a matrícula e retorna seus dados
+	def consulta_mat(matricula)
+		db = SQLite3::Database.open('xmountain.db')
+    db.results_as_hash = true
+    result = db.execute("select * from atletas where matricula = #{matricula}") 
+ #   return result
+	end
+  
+  def consulta_cat(categoria)
+    puts categoria
+		db = SQLite3::Database.open('xmountain.db')
+    result = db.execute("select matricula from atletas where categoria = '#{categoria}'") 
+  end
+
   def importa(source)
     workbook = Roo::Spreadsheet.open(source)
 
@@ -24,12 +66,12 @@ require 'roo'
 		  # Get the column data using the column heading.
 		  matricula = workbook.row(row)[headers['Matrícula']]
 		  tempo = (workbook.row(row)[headers['Tempo']])
-		  
-		  print "Row: #{matricula}, #{tempo}\n\n" #.strftime("%H:%M:%S")
+      # Fetch athlete data (name and phone) from DB
+      res  = consulta_mat(matricula)
+      pos = classifica(consulta_cat(res[0]['categoria']))  
+      sendSms(res[0]['nome'], res[0]['categoria'], tempo,res[0]['fone'], pos)
 		end  
 	end
-puts "Importando"
-  importa('./results.xlsx')
   
 =begin    worksheets.each do |worksheet|
       puts "Reading: #{worksheet}"
@@ -47,6 +89,7 @@ puts "Importando"
 
     puts 'Done'  
   end
+=end
 
 	def criadb
 		db = SQLite3::Database.new('xmountain.db')
@@ -68,23 +111,11 @@ puts "Importando"
 
 		if db.execute("select * from atletas where matricula==#{values[0]}").empty?
 				db.execute("INSERT INTO atletas(matricula, nome, categoria, fone) VALUES(?,?,?,?)", values)
-		else
-				puts "Atleta já existe"
 				puts values
-				puts " "
 		end
 	end
-
-	def consulta
-		db = SQLite3::Database.open('xmountain.db')
-    db.execute("select * from atletas") do |result|
-    	puts 'Atleta '
-    	puts result
-    	puts " "
-    end
-	end
-  
- # res = criadb
+ 
+#  res = criadb
   values = [[
     "1702",
 		"Marya Lima",
@@ -114,40 +145,62 @@ puts "Importando"
     "Tomaz Correa",
     "Expert",
     "3199500083"
+  ],
+ [
+    "1710",
+    "Jose Silva",
+    "master c",
+    "31992320098"
+  ],
+  [
+    "1716",
+    "Fred Antonio Salgado",
+    "cadete",
+    "31996950250"
+  ],
+  [
+    "1715",
+    "Raphael Severo Souza",
+    "master b",
+    "31998854354"
+  ],
+  [
+    '1714',
+    "Henrik Jailton Souza",
+    "Expert",
+    "31984954144"
+  ] 
   ]
-  ]
-#  values.each {|item| insere(item)}
-=end#	consulta	
+  values.each {|item| insere(item)}
 	puts "Importando"
   importa('./results.xlsx')
 
-end
-=begin
-	
-rescue Exception => e
-	
-end
-username = 'jbetol'
-password = 'Petom@123'
-msisdn = ['31992320098','31992320104','31993500083']
-msg = 'XMountain Informa'
+#end
 
-uri = URI('https://sms4geeks.appspot.com/smsgateway')#ction=out&username=YourUserName&password=YourPassword&msisdn=555123456&msg=hello')
-
-msisdn.each do |cel|
-  request = Net::HTTP::Post.new(uri)
-	request.set_form_data('action' => 'out', 'username' => username, 'password' => password,
-		     		'msisdn' => cel, 'msg' => msg)
-
-	res = Net::HTTP.start(uri.hostname,  uri.port, :use_ssl => uri.scheme == 'https') do |http|
-		
-		http.request(request)
-	#	puts "Res " + res.body
-	end	
-end
 		#URI.parse('https://sms4geeks.appspot.com/smsgateway'),
 	  #   {'action' => 'out', 'username' => username, 'password' => password,
 	  #   		'msisdn' => msisdn, 'msg' => msg})
 
 #p http.status
+=begin
+MASTER CM
+MASTER CF
+30 km MASTER DM
+30 km MASTER DF
+30 km PNE M
+      PNE F  
+30 km SPORT F
+30 km SPORT MA
+30 km SPORT MB
+30 km T-REX
+48 km CADETE
+48 km ELITE M
+48 km MASTER 30F
+48 km MASTER A1
+48 km MASTER A2
+48 km MASTER B1
+48 km MASTER B2
+48 km SUB 30F
+48 km SUB 30M
+DUPLA MISTA 
 =end
