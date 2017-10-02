ce07950072cce74399a0eb269207aa879ee28db4
Joao Roberto de Lima <jbetol@gmail.com>
2017-10-02 20:06:37 -0300

    Adding categories table

--- sms4geek.rb
+++ sms4geek.rb
@@ -2,10 +2,12 @@ require 'net/http'
 require 'sqlite3'
 require 'roo'
 
+#Inicialização de variáveis 
 @xmountain_DB = './db/xmountain.db'
-@planilha_resultados = './db/Listagem.xlsx'
+@planilha_resultados = './db/'+ ARGV[0] #Listagem.xlsx'
+@planilha_categorias = './db/categorias.ods'
 @qnt_atletas_categoria = Hash.new(0)
-@clas_atual_categoria = Hash.new(0)
+@categorias = Hash.new(0)
 @db = nil 
 
 def inicializaAmbiente
@@ -16,6 +18,18 @@ end
 #Inicializa hash de categorias e de classificação
 def inicializaCategorias
     puts "Inicializando categorias..."
+
+#Initializing categories table/hash    
+  lista_categorias = Roo::Spreadsheet.open(@planilha_categorias)
+
+  lista_categorias.default_sheet = lista_categorias.sheets[0]
+
+# Create a hash of the categories code and name
+    ((lista_categorias.first_row + 1)..lista_categorias.last_row).each do |col|
+        @categorias[col[1]] = col[2]
+    end      
+    puts @categorias
+    
     result = @db.execute("select categoria from atletas") 
 
     result.each do |cat|
@@ -29,13 +43,15 @@ def sendSms(atleta, categoria, tempo, fone, poscat, posgeral)
 
   username = 'jbetol'
   password = 'Petom@123'
-  msisdn = fone.gsub!(/\D/,"")
-  msg = "#{atleta}, XMountain informa seu resultado:
-  Categoria => #{categoria}
-  Tempo de prova => #{tempo}
-  Classificacao categoria parcial => #{poscat}/#{@qnt_atletas_categoria[categoria]}
-  Classificacao geral parcial => #{posgeral}/#{@qnt_atletas_categoria[categoria]}"
-
+  msisdn = fone.gsub!(/\D/,"") #retira o que não for número do telefone
+  msg = "
+        #{atleta.split[0]}, XMountain informa seu resultado:
+        Categoria => #{categoria}
+        Tempo de prova => #{tempo.strftime("%HH:%MM:%SS")}
+        Classificacao categoria parcial => #{poscat}/#{@qnt_atletas_categoria[categoria]}
+        Classificacao geral parcial => #{posgeral}/#{@qnt_atletas_categoria[categoria]}
+        "
+  
     puts msg 
     uri = URI('https://sms4geeks.appspot.com/smsgateway')#ction=out&username=YourUserName&password=YourPassword&msisdn=555123456&msg=hello')
 
@@ -43,9 +59,8 @@ def sendSms(atleta, categoria, tempo, fone, poscat, posgeral)
   	request.set_form_data('action' => 'out', 'username' => username, 'password' => password,
   		     		'msisdn' => msisdn, 'msg' => msg)
 
-  	res = Net::HTTP.start(uri.hostname,  uri.port, :use_ssl => uri.scheme == 'https') do |http|
-  		
-    #	http.request(request)
+  	res = Net::HTTP.start(uri.hostname,  uri.port, :use_ssl => uri.scheme == 'http') do |http|
+#        http.request(request)
   	end	
 end
 
@@ -54,30 +69,35 @@ def classifica(categoria,tempo)
 end
   
 #Consulta atleta dada a matrícula e retorna seus dados
-	def consulta_mat(matricula)
-    result = @db.execute("select * from atletas where matricula = #{matricula}") 
-    return result
-	end
+def consulta_mat(matricula)
+  result = @db.execute("select * from atletas where matricula = #{matricula}") 
+  return result
+end
 
-  def importa#(source)
-    workbook = Roo::Spreadsheet.open(@planilha_resultados)
+def preparaEnvioSMS
 
-    workbook.default_sheet = workbook.sheets[0]
+  workbook = Roo::Spreadsheet.open(@planilha_resultados)
+
+  workbook.default_sheet = workbook.sheets[0]
 
 # Create a hash of the headers so we can access columns by name (assuming row
 # 1 contains the column headings).  This will also grab any data in hidden
 # columns.
-		headers = Hash.new
-		workbook.row(1).each_with_index {|header,i|
-  		headers[header] = i
-		}
+  headers = Hash.new
+  workbook.row(1).each_with_index {|header,i|
+    headers[header] = i
+  }
+  
+# Iterate over courses PRO and Sport
+  0.upto(1) do |i|
+    workbook.default_sheet = workbook.sheets[i] # Setting default sheet for PRO and SPORT
 
 # Iterate over the rows using the `first_row` and `last_row` methods.  Skip
 # the header row in the range.
-		((workbook.first_row + 1)..workbook.last_row).each do |row|
+    ((workbook.first_row + 1)..workbook.last_row).each do |row|
 
-		  # Get the column data using the column heading.
-		  matricula = workbook.row(row)[headers['PLAQUETA']]
+# Get the column data using the column heading.
+#     matricula = workbook.row(row)[headers['PLAQUETA']]
 #		  tempo = (workbook.row(row)[headers['Tempo']])
 
     # Fetch athlete data (name and phone) from DB, if do not use the Results_real spreadsheet
@@ -85,22 +105,54 @@ end
       #  pos = classifica(res[0]['categoria'], tempo)   
       #  sendSms(res[0]['nome'], res[0]['categoria'], tempo,res[0]['fone'],pos)
 
-     #Fetch all data directly from spreadsheet Results_real
+#Fetch all data directly from spreadsheet Results_real
       sendSms(workbook.row(row)[headers['NOME']], 
               workbook.row(row)[headers['COD CAT']],
-              workbook.row(row)[headers['TEMPO PROVA']].strftime("%HH:%MM:%SS"),
+              workbook.row(row)[headers['TEMPO PROVA']],
               workbook.row(row)[headers['Celular']].to_s,
               workbook.row(row)[headers['POSIÇAO CAT']],
               workbook.row(row)[headers['POSIÇAO GERAL']])
 
-		end  
-	end
-  
+    end 
+  end      
+  workbook.close
+end
+
 #	Criando e configurando BD
-  def inicializaDB
-    puts "Inicializando DB..."
+def inicializaDB
+  puts "Inicializando DB..."
+
+  #Verifica se planilha de resultados já está no local apropriado   
+  if !File.exist?@planilha_resultados
+    puts "#{@planilha_resultados} não encontrada em " + File.dirname(@planilha_resultados) + "... saindo..."
+    exit     
+  else
+    puts "Planilha Ok."
+  end
+  
+#Inicializa banco de dados e tabelas, caso necessários    
+  if !File.exist?@xmountain_DB
+    puts "Banco de dados não encontrado em " + File.dirname(@xmountain_DB) + "... saindo..."
+    exit     
+  else
+    puts "Abrindo DB..."
+    @db = SQLite3::Database.open(@xmountain_DB)
+    @db.results_as_hash = true
+  end
+end
+inicializaAmbiente#(xmountain_DB,qnt_atletas_categoria)
+
+#  res = criadb
+#  values.each {|item| insere(item)}
+preparaEnvioSMS
+@db.close
+puts "Saindo em paz..."
+ 
+  
+  
+=begin
 		if @db.nil?
-      puts "Criado DB..."
+      puts "Criando DB..."
       @db = SQLite3::Database.new(@xmountain_DB) 
       @db.execute <<-SQL
 							create table IF NOT EXISTS atletas(
@@ -110,14 +162,7 @@ end
 							 	fone varchar(14)
 							 	);
 							 	SQL
-      puts "Abrindo DB..."
-      @db = SQLite3::Database.open(@xmountain_DB)
-    else
-      puts "Abrindo DB..."
-      @db = SQLite3::Database.open(@xmountain_DB)
-    end
-    @db.results_as_hash = true
-
+end
 	end
 
 # Popula tabela de atletas
@@ -127,86 +172,4 @@ end
         puts "Inserindo atletas no DB..."
 		end
 	end
-
-  inicializaAmbiente#(xmountain_DB,qnt_atletas_categoria)
- 
-#  res = criadb
-  values = [[
-    "1702",
-		"Marya Lima",
-    "master a",
-    "31992320104"
-  ],
-  [
-    "1701",
-    "Joao Lima",
-    "master c",
-    "31992320098"
-  ],
-  [
-    "1704",
-    "Pedro Lima",
-    "cadete",
-    "31992320162"
-  ],
-  [
-    "1705",
-    "Rayla Lima",
-    "master b",
-    "31993500114"
-  ],
-  [
-    "1703",
-    "Tomaz Correa",
-    "Expert",
-    "3199500083"
-  ],
- [
-    "1710",
-    "Jose Silva",
-    "master c",
-    "31992320098"
-  ],
-  [
-    "1716",
-    "Fred Antonio Salgado",
-    "cadete",
-    "31996950250"
-  ],
-  [
-    "1715",
-    "Raphael Severo Souza",
-    "master b",
-    "31998854354"
-  ],
-  [
-    "1714",
-    "Henrik Jailton Souza",
-    "Expert",
-    "31984954144"
-  ],
-    [
-    "1707",
-    "Caca Lima",
-    "cadete",
-    "31992320098"
-  ],
-  [
-    "1709",
-    "Deusdete da Silva",
-    "master b",
-    "31993500114"
-  ],
-  [
-    "1708",
-    "Lula",
-    "Expert",
-    "3199500083"
-  ] 
-  ]
-  values.each {|item| insere(item)}
-	importa#(@planilha_resultados)
-  puts @clas_atual_categoria
-  @db.close
-  puts "DB fechado..."
-
+=end
