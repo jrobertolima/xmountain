acfd46eea818c47a42ac642f2a8958807d7b2e30
Joao Roberto de Lima <jbetol@gmail.com>
2017-10-03 20:14:44 -0300

    Sending generic sms

--- sms4geek.rb
+++ sms4geek.rb
@@ -1,13 +1,19 @@
 require 'net/http'
+require 'openssl'
 require 'sqlite3'
 require 'roo'
 
+#OpenSSL::SSL::VERIFY_PEER = OpenSSL::SSL::VERIFY_NONE
 #Inicialização de variáveis 
+# Files application
 @xmountain_DB = './db/xmountain.db'
-@planilha_resultados = './db/'+ ARGV[0] #Listagem.xlsx'
+@planilha_resultados = './db/Listagem.xlsx'
 @planilha_categorias = './db/categorias.ods'
+
+#globals variable
 @qnt_atletas_categoria = Hash.new(0)
 @categorias = Hash.new(0)
+@qnt_atletas_total = 0
 @db = nil 
 
 def inicializaAmbiente
@@ -17,7 +23,7 @@ end
 
 #Inicializa hash de categorias e de classificação
 def inicializaCategorias
-    puts "Inicializando categorias..."
+  puts "Inicializando categorias..."
 
 #Initializing categories table/hash    
   lista_categorias = Roo::Spreadsheet.open(@planilha_categorias)
@@ -25,32 +31,40 @@ def inicializaCategorias
   lista_categorias.default_sheet = lista_categorias.sheets[0]
 
 # Create a hash of the categories code and name
-    ((lista_categorias.first_row + 1)..lista_categorias.last_row).each do |col|
-        @categorias[col[1]] = col[2]
+    ((lista_categorias.first_row + 1)..lista_categorias.last_row).each do |row|
+        @categorias[lista_categorias.row(row)[0]] = lista_categorias.row(row)[1]
     end      
-    puts @categorias
     
+# Create a hash for number of athletes for each category    
     result = @db.execute("select categoria from atletas") 
-
     result.each do |cat|
       @qnt_atletas_categoria[cat[0]] += 1
-      @clas_atual_categoria[cat[0]] = 0
+      @qnt_atletas_total += 1
     end
-    puts "Categorias done!"
 end
 
-def sendSms(atleta, categoria, tempo, fone, poscat, posgeral)
+def preparaEnvioSMSGenerico
+
+  res = @db.execute("select * from atletas where matricula>1710")
+  res.each do |atleta|
+    msg = "
+          Caro #{atleta['nome'].split[0]}, 
+          XMOUNTAIN agradece sua inscricao na
+          categoria #{atleta['categoria']}.
+          Aguardamos voce em Lagoa Santa no 
+          dia 22OUT aas 10h.
+          Desejamos uma BOA PROVA.       
+          www.xmountain.com.br
+          "
+    sendSmsGenerico(msg,atleta[3])
+  end
+end
+
+def sendSmsGenerico(msg, fone)
 
   username = 'jbetol'
   password = 'Petom@123'
   msisdn = fone.gsub!(/\D/,"") #retira o que não for número do telefone
-  msg = "
-        #{atleta.split[0]}, XMountain informa seu resultado:
-        Categoria => #{categoria}
-        Tempo de prova => #{tempo.strftime("%HH:%MM:%SS")}
-        Classificacao categoria parcial => #{poscat}/#{@qnt_atletas_categoria[categoria]}
-        Classificacao geral parcial => #{posgeral}/#{@qnt_atletas_categoria[categoria]}
-        "
   
     puts msg 
     uri = URI('https://sms4geeks.appspot.com/smsgateway')#ction=out&username=YourUserName&password=YourPassword&msisdn=555123456&msg=hello')
@@ -59,23 +73,89 @@ def sendSms(atleta, categoria, tempo, fone, poscat, posgeral)
   	request.set_form_data('action' => 'out', 'username' => username, 'password' => password,
   		     		'msisdn' => msisdn, 'msg' => msg)
 
-  	res = Net::HTTP.start(uri.hostname,  uri.port, :use_ssl => uri.scheme == 'http') do |http|
-#        http.request(request)
+  	res = Net::HTTP.start(uri.hostname,  uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request(request)
   	end	
 end
 
-def classifica(categoria,tempo)
-  @clas_atual_categoria[categoria]+=1
-end
+#Send athlete result
+def sendSmsResultado(atleta, categoria, tempo, fone, poscat, posgeral)
+
+  username = 'jbetol'
+  password = 'Petom@123'
+  msisdn = '992320098'#fone.gsub!(/\D/,"") #retira o que não for número do telefone
+  msg = "
+        #{atleta.split[0]}, XMountain informa seu resultado:
+        Categoria => #{@categorias[categoria]}
+        Tempo de prova => #{tempo.strftime("%HH:%MM:%SS")}
+        Classificacao categoria parcial => #{poscat}/#{@qnt_atletas_categoria[@categorias[categoria]]}
+        Classificacao geral parcial => #{posgeral}/#{@qnt_atletas_total}
+        "
   
-#Consulta atleta dada a matrícula e retorna seus dados
-def consulta_mat(matricula)
-  result = @db.execute("select * from atletas where matricula = #{matricula}") 
-  return result
+#    puts msg 
+    uri = URI('https://sms4geeks.appspot.com/smsgateway')#ction=out&username=YourUserName&password=YourPassword&msisdn=555123456&msg=hello')
+
+    request = Net::HTTP::Post.new(uri)
+  	request.set_form_data('action' => 'out', 'username' => username, 'password' => password,
+  		     		'msisdn' => msisdn, 'msg' => msg)
+
+  	res = Net::HTTP.start(uri.hostname,  uri.port, :use_ssl => uri.scheme == 'https') do |http|
+        http.request(request)
+  	end	
 end
 
-def preparaEnvioSMS
+def preparaEnvioSMSResultado
 
+  workbook = Roo::Spreadsheet.open(@planilha_resultados)
+
+  workbook.default_sheet = workbook.sheets[0]
+
+# Create a hash of the headers so we can access columns by name (assuming row
+# 1 contains the column headings).  This will also grab any data in hidden
+# columns.
+  headers = Hash.new
+  workbook.row(1).each_with_index {|header,i|
+    headers[header] = i
+  }
+  
+# Iterate over courses PRO and Sport
+      mandauma = true 
+  0.upto(1) do |i|
+    workbook.default_sheet = workbook.sheets[i] # Setting default sheet for PRO and SPORT
+
+# Iterate over the rows using the `first_row` and `last_row` methods.  Skip
+# the header row in the range.
+    ((workbook.first_row + 1)..workbook.last_row).each do |row|
+
+# Get the column data using the column heading.
+#     matricula = workbook.row(row)[headers['PLAQUETA']]
+#		  tempo = (workbook.row(row)[headers['Tempo']])
+
+    # Fetch athlete data (name and phone) from DB, if do not use the Results_real spreadsheet
+      #  res  = consulta_mat(matricula)
+      #  pos = classifica(res[0]['categoria'], tempo)   
+      #  sendSms(res[0]['nome'], res[0]['categoria'], tempo,res[0]['fone'],pos)
+
+#Fetch all data directly from spreadsheet Results_real
+      if mandauma
+         sendSmsResultado(workbook.row(row)[headers['NOME']], 
+              workbook.row(row)[headers['COD CAT']],
+              workbook.row(row)[headers['TEMPO PROVA']],
+              workbook.row(row)[headers['Celular']].to_s,
+              workbook.row(row)[headers['POSIÇAO CAT']],
+              workbook.row(row)[headers['POSIÇAO GERAL']])
+      mandauma = false
+          puts "mandou"
+       end
+    end 
+  end      
+  workbook.close
+end
+
+# Popula tabela de atletas no banco de dados XMountain
+def insereAtleta
+  
+  atleta =[]  
   workbook = Roo::Spreadsheet.open(@planilha_resultados)
 
   workbook.default_sheet = workbook.sheets[0]
@@ -97,32 +177,26 @@ def preparaEnvioSMS
     ((workbook.first_row + 1)..workbook.last_row).each do |row|
 
 # Get the column data using the column heading.
-#     matricula = workbook.row(row)[headers['PLAQUETA']]
-#		  tempo = (workbook.row(row)[headers['Tempo']])
-
-    # Fetch athlete data (name and phone) from DB, if do not use the Results_real spreadsheet
-      #  res  = consulta_mat(matricula)
-      #  pos = classifica(res[0]['categoria'], tempo)   
-      #  sendSms(res[0]['nome'], res[0]['categoria'], tempo,res[0]['fone'],pos)
-
-#Fetch all data directly from spreadsheet Results_real
-      sendSms(workbook.row(row)[headers['NOME']], 
-              workbook.row(row)[headers['COD CAT']],
-              workbook.row(row)[headers['TEMPO PROVA']],
-              workbook.row(row)[headers['Celular']].to_s,
-              workbook.row(row)[headers['POSIÇAO CAT']],
-              workbook.row(row)[headers['POSIÇAO GERAL']])
-
+        atleta[0] = workbook.row(row)[headers['PLAQUETA']]
+        atleta[1] = workbook.row(row)[headers['NOME']]
+        atleta[2] = @categorias[workbook.row(row)[headers['COD CAT']]]
+        atleta[3] = workbook.row(row)[headers['Celular']].to_s
+# Inclui atleta na tabela        
+        if @db.execute("select * from atletas where matricula==#{atleta[0]}").empty? #Atleta não existe, então inclui
+            @db.execute("INSERT INTO atletas(matricula, nome, categoria, fone) VALUES(?,?,?,?)", atleta)
+            puts "Inserindo atletas no DB..."
+        end
     end 
   end      
   workbook.close
-end
+
+	end
 
 #	Criando e configurando BD
 def inicializaDB
   puts "Inicializando DB..."
 
-  #Verifica se planilha de resultados já está no local apropriado   
+#Verifica se planilha de resultados já está no local apropriado   
   if !File.exist?@planilha_resultados
     puts "#{@planilha_resultados} não encontrada em " + File.dirname(@planilha_resultados) + "... saindo..."
     exit     
@@ -143,8 +217,9 @@ end
 inicializaAmbiente#(xmountain_DB,qnt_atletas_categoria)
 
 #  res = criadb
-#  values.each {|item| insere(item)}
-preparaEnvioSMS
+#insereAtleta # In order to prepare xmountain_DB
+preparaEnvioSMSResultado
+preparaEnvioSMSGenerico
 @db.close
 puts "Saindo em paz..."
  
@@ -164,12 +239,6 @@ puts "Saindo em paz..."
 							 	SQL
 end
 	end
-
-# Popula tabela de atletas
-	def insere(atleta)
-		if @db.execute("select * from atletas where matricula==#{atleta[0]}").empty? #Atleta não existe, então inclui
-				@db.execute("INSERT INTO atletas(matricula, nome, categoria, fone) VALUES(?,?,?,?)", atleta)
-        puts "Inserindo atletas no DB..."
-		end
-	end
+Set environment for SSL 
+set SSL_CERT_FILE=C:\Users\88630447753\Developer\RailsInstaller\cacert.pem 
 =end
